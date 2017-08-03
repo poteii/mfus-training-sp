1791839d1e1206dfdee1993973ec88d2e1319f35
poteii <poteii.pasel@icloud.com>
2017-08-03 13:53:34 +0700

    commit form

---
+++ mfus-training-sp/src/main/java/com/mfus/training/form/G00RegisterForm.java
@@ -0,0 +1,12 @@
+package com.mfus.training.form;
+
+import java.io.Serializable;
+
+public class G00RegisterForm implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
