b93fdcd2a4e658973fc055dbebbed46bd94386d3
Palm <5831305015@lamduan.mfu.ac.th>
2017-08-04 11:40:01 +0700

    5015_Palm

--- mfus-training-sp/src/main/java/com/mfus/training/entity/G05Sponsor.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G05Sponsor.java
@@ -1,8 +1,15 @@
 package com.mfus.training.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 
 
 /**
