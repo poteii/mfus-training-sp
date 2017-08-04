8bd719391031dee845013e393b827d0c8b973dab
Patikon Ruang-in <ultimate_last@hotmail.com>
2017-08-04 11:28:57 +0700

    5028_Knes

--- mfus-training-sp/src/main/java/com/mfus/training/entity/G03Officer.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G03Officer.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 @NamedQuery(name="G03Officer.findAll", query="SELECT g FROM G03Officer g")
 public class G03Officer implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	@Column(name="officer_ref")
