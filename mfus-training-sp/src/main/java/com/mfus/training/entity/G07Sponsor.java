679c251d2345c61bbcf1b39a73d154f8fd51dae2
Rosena <nattawipa87@gmail.com>
2017-08-04 12:01:28 +0700

    5017_Rosena

--- mfus-training-sp/src/main/java/com/mfus/training/entity/G07Sponsor.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G07Sponsor.java
@@ -1,8 +1,14 @@
 package com.mfus.training.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
 /**
@@ -14,6 +20,7 @@ import java.sql.Timestamp;
 @NamedQuery(name="G07Sponsor.findAll", query="SELECT g FROM G07Sponsor g")
 public class G07Sponsor implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	@Column(name="sponsors_id")
