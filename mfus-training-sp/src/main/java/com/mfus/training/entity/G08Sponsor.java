70707d9bf4c2ee3e3c70f11ba517ac7a73d12769
Nichakan Chinsathapornchok <aum259259@gmail.com>
2017-08-04 11:45:37 +0700

    1014_Aum

--- mfus-training-sp/src/main/java/com/mfus/training/entity/G08Sponsor.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G08Sponsor.java
@@ -1,8 +1,14 @@
 package com.mfus.training.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
 /**
@@ -14,6 +20,7 @@ import java.sql.Timestamp;
 @NamedQuery(name="G08Sponsor.findAll", query="SELECT g FROM G08Sponsor g")
 public class G08Sponsor implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	@Column(name="sponsors_id")
