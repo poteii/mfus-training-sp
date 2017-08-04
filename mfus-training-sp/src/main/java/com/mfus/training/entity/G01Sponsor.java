6c021355cbde9d5c922c3582dc7f05fb9452744d
Anda Nachai <tn.pechii@gmail.com>
2017-08-04 11:22:47 +0700

    5061_Anda

--- mfus-training-sp/src/main/java/com/mfus/training/entity/G01Sponsor.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G01Sponsor.java
@@ -1,8 +1,14 @@
 package com.mfus.training.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
 /**
@@ -221,5 +227,5 @@ public class G01Sponsor implements Serializable {
 	public void setWebsite(String website) {
 		this.website = website;
 	}
-
+ 
 }
