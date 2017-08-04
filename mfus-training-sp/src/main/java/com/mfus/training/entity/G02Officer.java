1c266e66c719454b4b79f786633e931f1cc145cc
ICreator59 <fulkza191@gmail.com>
2017-08-04 11:09:13 +0700

    5059_fluke

---
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G02Officer.java
@@ -0,0 +1,268 @@
+package com.mfus.training.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the g02_officer database table.
+ * 
+ */
+@Entity
+@Table(name="g02_officer")
+@NamedQuery(name="G02Officer.findAll", query="SELECT g FROM G02Officer g")
+public class G02Officer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="officer_ref")
+	private int officerRef;
+
+	@Column(name="active_flag")
+	private String activeFlag;
+
+	private String address;
+
+	@Column(name="create_datetime")
+	private Timestamp createDatetime;
+
+	@Column(name="create_user")
+	private int createUser;
+
+	private String district;
+
+	private String email;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	private String gender;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	private String manage_officer_flag;
+
+	@Column(name="officer_code")
+	private String officerCode;
+
+	@Column(name="personal_id")
+	private String personalId;
+
+	@Column(name="phone_no")
+	private String phoneNo;
+
+	private String postcode;
+
+	@Lob
+	@Column(name="profile_image")
+	private byte[] profileImage;
+
+	@Column(name="profile_name")
+	private String profileName;
+
+	@Column(name="profile_type")
+	private String profileType;
+
+	private String province;
+
+	private String sub_district;
+
+	@Column(name="title_code")
+	private String titleCode;
+
+	@Column(name="update_datetime")
+	private Timestamp updateDatetime;
+
+	@Column(name="update_user")
+	private int updateUser;
+
+	public G02Officer() {
+	}
+
+	public int getOfficerRef() {
+		return this.officerRef;
+	}
+
+	public void setOfficerRef(int officerRef) {
+		this.officerRef = officerRef;
+	}
+
+	public String getActiveFlag() {
+		return this.activeFlag;
+	}
+
+	public void setActiveFlag(String activeFlag) {
+		this.activeFlag = activeFlag;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Timestamp getCreateDatetime() {
+		return this.createDatetime;
+	}
+
+	public void setCreateDatetime(Timestamp createDatetime) {
+		this.createDatetime = createDatetime;
+	}
+
+	public int getCreateUser() {
+		return this.createUser;
+	}
+
+	public void setCreateUser(int createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getDistrict() {
+		return this.district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getGender() {
+		return this.gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getManage_officer_flag() {
+		return this.manage_officer_flag;
+	}
+
+	public void setManage_officer_flag(String manage_officer_flag) {
+		this.manage_officer_flag = manage_officer_flag;
+	}
+
+	public String getOfficerCode() {
+		return this.officerCode;
+	}
+
+	public void setOfficerCode(String officerCode) {
+		this.officerCode = officerCode;
+	}
+
+	public String getPersonalId() {
+		return this.personalId;
+	}
+
+	public void setPersonalId(String personalId) {
+		this.personalId = personalId;
+	}
+
+	public String getPhoneNo() {
+		return this.phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getPostcode() {
+		return this.postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public byte[] getProfileImage() {
+		return this.profileImage;
+	}
+
+	public void setProfileImage(byte[] profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public String getProfileName() {
+		return this.profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getProfileType() {
+		return this.profileType;
+	}
+
+	public void setProfileType(String profileType) {
+		this.profileType = profileType;
+	}
+
+	public String getProvince() {
+		return this.province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getSub_district() {
+		return this.sub_district;
+	}
+
+	public void setSub_district(String sub_district) {
+		this.sub_district = sub_district;
+	}
+
+	public String getTitleCode() {
+		return this.titleCode;
+	}
+
+	public void setTitleCode(String titleCode) {
+		this.titleCode = titleCode;
+	}
+
+	public Timestamp getUpdateDatetime() {
+		return this.updateDatetime;
+	}
+
+	public void setUpdateDatetime(Timestamp updateDatetime) {
+		this.updateDatetime = updateDatetime;
+	}
+
+	public int getUpdateUser() {
+		return this.updateUser;
+	}
+
+	public void setUpdateUser(int updateUser) {
+		this.updateUser = updateUser;
+	}
+
+}
