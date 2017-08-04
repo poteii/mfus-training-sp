cc69fd77401db9e2be5bcf646ffca3b3311ba40e
poteii <poteii.pasel@icloud.com>
2017-08-04 16:59:48 +0700

    test fetch data by poteii

--- mfus-training-sp/src/main/java/com/mfus/training/entity/RftDistrict.java
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/RftDistrict.java
@@ -1,8 +1,13 @@
 package com.mfus.training.entity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
 /**
@@ -14,6 +19,7 @@ import java.sql.Timestamp;
 @NamedQuery(name="RftDistrict.findAll", query="SELECT r FROM RftDistrict r")
 public class RftDistrict implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	@Column(name="district_code")
