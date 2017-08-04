539449178a0eb1f36dda977d11246cd9f53417fd
poteii <poteii.pasel@icloud.com>
2017-08-04 17:15:37 +0700

    poteii

--- mfus-training-sp/src/main/java/com/mfus/training/form/G00RegisterForm.java
+++ mfus-training-sp/src/main/java/com/mfus/training/form/G00RegisterForm.java
@@ -2,11 +2,34 @@ package com.mfus.training.form;
 
 import java.io.Serializable;
 
+import com.mfus.training.entity.G00Student;
+import com.mfus.training.entity.G00User;
+
 public class G00RegisterForm implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	//for insert
+	private G00Student 		g00Student;
+	private G00User 		g00User;
+	
+	
+	public G00Student getG00Student() {
+		return g00Student;
+	}
+	public void setG00Student(G00Student g00Student) {
+		this.g00Student = g00Student;
+	}
+	public G00User getG00User() {
+		return g00User;
+	}
+	public void setG00User(G00User g00User) {
+		this.g00User = g00User;
+	}
+	
+	
+	
 
 }
