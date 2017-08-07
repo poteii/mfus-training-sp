6f497beb3a52da371bf2de976cac924f9667283a
poteii <poteii.pasel@icloud.com>
2017-08-07 16:03:26 +0700

    change return ResponseEntity by poteii

--- mfus-training-sp/src/main/java/com/mfus/training/controller/G00RegisterController.java
+++ mfus-training-sp/src/main/java/com/mfus/training/controller/G00RegisterController.java
@@ -1,6 +1,8 @@
 package com.mfus.training.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +22,18 @@ public class G00RegisterController {
 	// angular send url => 
 	// http://localhost:8080/mfus-training-sp/g00Register/createStudent
 	@RequestMapping(value="/createStudent")
-	public void createStudent(@RequestBody G00RegisterForm form) {
+	//step 4 change void method to return ResponsEntity
+	public ResponseEntity createStudent(@RequestBody G00RegisterForm form) {
 		//step 3
-		g00RegisterService.createStudent(form);
+		//step 5 add try catch
+		try {
+			g00RegisterService.createStudent(form);
+			//step 6 return status
+			return new ResponseEntity("insert success",HttpStatus.OK);
+		}catch (Exception e) {
+			//step 7 return error message
+			return new ResponseEntity("error",HttpStatus.FAILED_DEPENDENCY);
+		} 
 	}
 
 }
