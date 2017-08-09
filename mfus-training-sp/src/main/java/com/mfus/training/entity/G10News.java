8a1a4d7b56519027914c74b201cf0ff4b24dd9a2
biwswalker <biwswalker@10.1.87.4>
2017-08-09 15:44:06 +0700

    entity

---
+++ mfus-training-sp/src/main/java/com/mfus/training/entity/G10News.java
@@ -0,0 +1,157 @@
+package com.mfus.training.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the g10_news database table.
+ * 
+ */
+@Entity
+@Table(name="g10_news")
+@NamedQuery(name="G10News.findAll", query="SELECT g FROM G10News g")
+public class G10News implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="news_ref")
+	private int newsRef;
+
+	@Column(name="active_flag")
+	private String activeFlag;
+
+	@Column(name="create_datetime")
+	private Timestamp createDatetime;
+
+	@Column(name="create_user")
+	private int createUser;
+
+	@Column(name="news_detail")
+	private String newsDetail;
+
+	@Lob
+	@Column(name="news_image")
+	private byte[] newsImage;
+
+	@Column(name="news_name")
+	private String newsName;
+
+	@Column(name="news_topic")
+	private String newsTopic;
+
+	@Column(name="news_type")
+	private String newsType;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="publish_date")
+	private Date publishDate;
+
+	@Column(name="update_datetime")
+	private Timestamp updateDatetime;
+
+	@Column(name="update_user")
+	private int updateUser;
+
+	public G10News() {
+	}
+
+	public int getNewsRef() {
+		return this.newsRef;
+	}
+
+	public void setNewsRef(int newsRef) {
+		this.newsRef = newsRef;
+	}
+
+	public String getActiveFlag() {
+		return this.activeFlag;
+	}
+
+	public void setActiveFlag(String activeFlag) {
+		this.activeFlag = activeFlag;
+	}
+
+	public Timestamp getCreateDatetime() {
+		return this.createDatetime;
+	}
+
+	public void setCreateDatetime(Timestamp createDatetime) {
+		this.createDatetime = createDatetime;
+	}
+
+	public int getCreateUser() {
+		return this.createUser;
+	}
+
+	public void setCreateUser(int createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getNewsDetail() {
+		return this.newsDetail;
+	}
+
+	public void setNewsDetail(String newsDetail) {
+		this.newsDetail = newsDetail;
+	}
+
+	public byte[] getNewsImage() {
+		return this.newsImage;
+	}
+
+	public void setNewsImage(byte[] newsImage) {
+		this.newsImage = newsImage;
+	}
+
+	public String getNewsName() {
+		return this.newsName;
+	}
+
+	public void setNewsName(String newsName) {
+		this.newsName = newsName;
+	}
+
+	public String getNewsTopic() {
+		return this.newsTopic;
+	}
+
+	public void setNewsTopic(String newsTopic) {
+		this.newsTopic = newsTopic;
+	}
+
+	public String getNewsType() {
+		return this.newsType;
+	}
+
+	public void setNewsType(String newsType) {
+		this.newsType = newsType;
+	}
+
+	public Date getPublishDate() {
+		return this.publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public Timestamp getUpdateDatetime() {
+		return this.updateDatetime;
+	}
+
+	public void setUpdateDatetime(Timestamp updateDatetime) {
+		this.updateDatetime = updateDatetime;
+	}
+
+	public int getUpdateUser() {
+		return this.updateUser;
+	}
+
+	public void setUpdateUser(int updateUser) {
+		this.updateUser = updateUser;
+	}
+
+}
